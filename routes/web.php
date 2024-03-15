e5b9edf64448c9b186ba37c3aa08eef177aaebc4
Jolver Andre Gil Quevedo <jolveragq@ext.massimodutti.com>
2024-03-15 14:28:09 +0100

    add api route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'api'], function () {
+    Route::get('/', function(){
+        return 'Api';
+    });
+});
+
 Route::get('{any}', function () {
     return view('frontend');
 })->where('any', '.*');
